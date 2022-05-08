5bcd79e383ee15d75df4218cde0ed2e9488387ee
jackshinshin <curryisalegend@gmail.com>
2022-05-08 17:56:51 +0800

    Adding validation for email when create a user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,9 +1,10 @@
+from multiprocessing.sharedctypes import Value
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 import inspect
 from django.core.management.base import BaseCommand
 class ModelTests(TestCase):
-    def test_create_user_with_email_successfull(self):
+    def test_create_user_with_email_successful(self):
         cmd = BaseCommand()
         cmd.stdout.write(f'--------Test {cmd.style.WARNING(inspect.currentframe().f_code.co_name)} begins--------')
         # Test creating a new user with successful email notice
@@ -17,6 +18,7 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         # because password is encrypted, it can only be checked in this way
         self.assertTrue(user.check_password(password))
+        
         cmd.stdout.write(cmd.style.SUCCESS('OK!'))
         
     def test_normalize_email(self):
@@ -28,7 +30,17 @@ class ModelTests(TestCase):
             'randomcharacters'
         )
         self.assertEqual(user.email, email.lower())
+        
         cmd.stdout.write(cmd.style.SUCCESS('OK!'))
+
+    def test_new_user_invalid_email(self):
+        cmd = BaseCommand()
+        cmd.stdout.write(f'--------Test {cmd.style.WARNING(inspect.currentframe().f_code.co_name)} begins--------')
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "tasdfds")
+            
+        cmd.stdout.write(cmd.style.SUCCESS('OK!'))
+        
     def test_create_superuser(self):
         cmd = BaseCommand()
         cmd.stdout.write(f'--------Test {cmd.style.WARNING(inspect.currentframe().f_code.co_name)} begins--------')
@@ -39,4 +51,5 @@ class ModelTests(TestCase):
         # is_superuser is included in the permissionmixin
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+        
         cmd.stdout.write(cmd.style.SUCCESS('OK!'))
