299dfccbe24226ba8304fb83d0ee91675208646d
jackshinshin <curryisalegend@gmail.com>
2022-05-08 20:26:36 +0800

    Adding test for listing users and changing necessary parts of the django default admin class

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,12 @@
-# from django.contrib import admin
+from django.contrib import admin
 
+# change some of the variable of the imported class
+from django.contrib.auth.admin import UserAdmin as BaseUA
 
+from core import models
+
+class UserAdmin(BaseUA):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,37 @@
+import inspect
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.core.management.base import BaseCommand
+class AdminSiteTests(TestCase):
+    # Create a setup function that runs before any tests do
+
+    def setUp(self):
+        self.cmd = BaseCommand()
+        # accessible to the class
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@gmail.com',
+            password = '123456'
+        )
+        # force_login makes it easier to test without having to manually log in to django admin page
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'test@gmail.com',
+            password = '123456',
+            name = 'Test'
+        )
+        # Default django admin requires a username to create a user
+        # But we seeks to provide only emails, so a few changes have to be made in the admin.py file
+    def test_users_listed(self):
+        # Test that users are listed on user page
+        
+        self.cmd.stdout.write(f'--------Test {self.cmd.style.WARNING(inspect.currentframe().f_code.co_name)} begins--------')
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+        self.cmd.stdout.write(self.cmd.style.SUCCESS('OK!'))
+        
