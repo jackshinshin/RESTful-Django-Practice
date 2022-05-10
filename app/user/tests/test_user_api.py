ed3fa442a2193a34d74fc05fa67c3bd97acb0376
jackshinshin <curryisalegend@gmail.com>
2022-05-11 06:36:26 +0800

    Adding tests for managing user api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -10,6 +10,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+CURRENT_USER_URL = reverse('user:me')
 
 # Helper function that can be used for different tests
 
@@ -140,5 +141,79 @@ class PublicUserAPITests(TestCase):
         res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.cmd.stdout.write(self.cmd.style.SUCCESS('OK!'))
+
+    def test_retrieve_user_unauth(self):
+        # Test that authtication is required for users
+
+        self.cmd.stdout.write(
+            f'--------Test {self.cmd.style.WARNING(inspect.currentframe().f_code.co_name)} begins--------')
+
+        res = self.client.get(CURRENT_USER_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        self.cmd.stdout.write(self.cmd.style.SUCCESS('OK!'))
+
+
+class PrivateUserAPITest(TestCase):
+    # Authentication is required before making requests to the api
+
+    def setUp(self) -> None:
+        self.user = create_user(
+            email='test@gmail.com',
+            password='test1233',
+            name='Test'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        self.cmd = BaseCommand()
+
+    def test_retrieve_profile_success(self):
+        # Test retrieving profile for logged in users
+        self.cmd.stdout.write(
+            f'--------Test {self.cmd.style.WARNING(inspect.currentframe().f_code.co_name)} begins--------')
+
+        res = self.client.get(CURRENT_USER_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+        self.cmd.stdout.write(self.cmd.style.SUCCESS('OK!'))
+
+    def test_post_me_not_allowed(self):
+        # Test that POST is not allowed on the me url
+
+        self.cmd.stdout.write(
+            f'--------Test {self.cmd.style.WARNING(inspect.currentframe().f_code.co_name)} begins--------')
+
+        res = self.client.post(CURRENT_USER_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        self.cmd.stdout.write(self.cmd.style.SUCCESS('OK!'))
+
+    def test_update_user_profile(self):
+        # Test updating th euser profile for authenticated user
+
+        self.cmd.stdout.write(
+            f'--------Test {self.cmd.style.WARNING(inspect.currentframe().f_code.co_name)} begins--------')
+
+        payload = {
+            'name': 'newone',
+            'password': 'anotherpassword'
+        }
+
+        res = self.client.patch(CURRENT_USER_URL, payload)
+
+        # Update the user from the latest data in the database after sending the patch request
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
 
         self.cmd.stdout.write(self.cmd.style.SUCCESS('OK!'))
